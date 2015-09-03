303d22e945323d3d87098528b513237bb0840a4f
Bastiaan Oosterhuis <bastiaanoosterhuis@hotmail.com>
2015-09-03 15:18:09 +0200

    We can now send data as well

--- communication/communication.c
+++ communication/communication.c
@@ -16,10 +16,27 @@
 #define MESSAGE_LENGTH	4
 #define START_CHAR		'a'
 #define STOP_CHAR		'b'
+
 int	demo_done;
 int	count;
 
 char message[MESSAGE_LENGTH] = {0};
+
+/*------------------------------------------------------------------
+ * send_message -- send an array of characters
+ * Author: Bastiaan Oosterhuis
+ *------------------------------------------------------------------
+ */
+void send_message(char msg[], int length)
+{
+	int i;
+	for(i = 0; i < length; i++){	
+		while((X32_rx_status & 0x1 ) == 0);
+	
+		X32_rx_data = msg[i];
+	}
+	
+}
 
 /*------------------------------------------------------------------
  * isr_rx -- receival interrupt service routine
@@ -33,46 +50,45 @@ void isr_rx(void)
 	static int sync = 0;
  	static char prev = STOP_CHAR;
 	char data = X32_rx_data;
-	/*Check if there is data in the input buffer
-	 */	
-	if((X32_rx_status & 0x1) == 1)
+
+	/*Start of a new message
+	 */
+	if(data == START_CHAR && receive_count == 0 && prev == STOP_CHAR)
 	{	
-		/*Start of a new message
-		 */
-		if(data == START_CHAR && receive_count == 0 && prev == STOP_CHAR)
-		{	
-			sync = 1; //We now have synched with a message
-			receive_count++;
+		sync = 1; //We now have synched with a message
+		receive_count++;
+	}
+	/*Place data in array
+	 */		
+	else if (receive_count > 0 && receive_count < MESSAGE_LENGTH-1)
+	{
+		message[receive_count - 1] = data;
+		receive_count++; 	
+	}		
+	/*Check if stop char is valid
+	 */		
+	else
+	{
+		//If the final received char is not equal to the stopchar -> error			
+		if((data != STOP_CHAR && sync != 0) | receive_count == 0){
+			//setMode(panic);
+			X32_display = 0xFFFF;
+			printf("PANIC\r\n");
 		}
-		/*Place data in array
-		 */		
-		else if (receive_count > 0 && receive_count < MESSAGE_LENGTH-1)
-		{
-			message[receive_count - 1] = data;
-			receive_count++; 	
-		}		
-		/*Check if stop char is valid
-		 */		
 		else
-		{
-			//If the final received char is not equal to the stopchar -> error			
-			if((data != STOP_CHAR && sync != 0) | receive_count == 0){
-				//setMode(panic);
-				X32_display = 0xFFFF;
-				printf("PANIC\r\n");
-			}
-			else
-			{	
-				X32_display = message[0];
-				printf("message: %s\r\n", message);
-			}
-			receive_count = 0;			
+		{	
+			//for test purposes:
+			X32_display = message[0];
+			printf("message: %s\r\n", message);
+			send_message(message, 2);
 		}
-		
-		prev = data;
+		receive_count = 0;			
 	}
 	
+	prev = data;
+	
 }
+
 
 /*------------------------------------------------------------------
  * delay -- busy-wait for ms milliseconds
@@ -89,26 +105,40 @@ void delay(int ms)
  * main -- do the demo
  *------------------------------------------------------------------
  */
-int main() 
-{
+
+/*------------------------------------------------------------------
+ * setup_uart_interrupts -- Setup the interrupts used for receiving data
+ * Author: Bastiaan Oosterhuis
+ *------------------------------------------------------------------
+ */
+
+void setup_uart_interrupts(){
+
 	/*
 		Attach an interrupt to the receival of a byte
 		Set the priority
 		Enable the interrupt
 	*/
-        SET_INTERRUPT_VECTOR(INTERRUPT_PRIMARY_RX, &isr_rx);
-        SET_INTERRUPT_PRIORITY(INTERRUPT_PRIMARY_RX, 10);
-        ENABLE_INTERRUPT(INTERRUPT_PRIMARY_RX);
+	SET_INTERRUPT_VECTOR(INTERRUPT_PRIMARY_RX, &isr_rx);
+	SET_INTERRUPT_PRIORITY(INTERRUPT_PRIMARY_RX, 10);
+	ENABLE_INTERRUPT(INTERRUPT_PRIMARY_RX);
 
+}
+
+int main() 
+{
+	setup_uart_interrupts();
 	/* 
 		Enable global interrupts
 	 */
-        ENABLE_INTERRUPT(INTERRUPT_GLOBAL); 
+    ENABLE_INTERRUPT(INTERRUPT_GLOBAL); 
+
 	X32_display = 0x0000;
 	demo_done = 0;
 	count = 0;
 	while (! demo_done) {
-		//printf("main: %d\r\n",);
+		//printf("Send message:\r\n");
+	//	send_message(message, 2);
 		delay(300);
 		
 	}
