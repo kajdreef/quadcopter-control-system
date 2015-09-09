2c9ce54e69f4b8f4750cd33029dcb51bae52aeec
Bastiaan Oosterhuis <bastiaanoosterhuis@hotmail.com>
2015-09-09 14:00:42 +0200

    Can now also encode and send messages

--- communication/main.c
+++ communication/main.c
@@ -7,13 +7,17 @@ int demo_done = 0;
 
 //Message types
 struct JS JS_mes;
-struct DAQ DAQ_mes;
+struct DAQ DAQ_mes = {808464433,808464433,808464433,808464433,808464433,808464433,808464433,808464433};
 struct ERR Err_mes;
 struct DEB Deb_mes;
 struct CON Contr_mes;
 
 //Buffer where the message is stored
 char message[sizeof(JS_mes)] = {0};
+
+//output buffer
+char output_buffer[sizeof(DAQ_mes)+2];
+
 //message type received;
 char message_type = '0';
 
@@ -29,20 +33,24 @@ int main()
 	 */
     ENABLE_INTERRUPT(INTERRUPT_GLOBAL); 
 
-	X32_display = 0x0000;
+	X32_display = 0x1111;
 	
-
 	while (! demo_done) {
 				
 		if(MESSAGE_FLAG == TRUE){
-			JS_mes.lift = 10;
-			printf("Message type: %c\r\n", message_type);
+			X32_display = message_type;
 			decode(message_type,message);
 			printf("lift: %d\r\n", JS_mes.lift);	
 			printf("cont: %d\r\n", Contr_mes.P1);
 			MESSAGE_FLAG = FALSE;
-			
+				
+			encode(DAQ_CHAR, output_buffer);
+		//	printf("output message: %s\r\n", output_buffer);
+			send_message(output_buffer, message_length(DAQ_CHAR));		
+				
 		}
+
+		
 	}
 	X32_display = 0x0000;
 
