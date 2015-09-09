b010f587cfd45f72d8243347280c3bc59eb72370
kajdreef <kaj.dreef@gmail.com>
2015-09-09 13:09:18 +0200

    Joystick is added to the PC side communication only now need to implement encode and sending of the joystick messages

--- PC/main.c
+++ PC/main.c
@@ -1,7 +1,18 @@
 #include <string.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <errno.h>
+
+#define DEBUG_SEND_MESSAGE 1
+
 #include "communication.h"
 
+#if !DEBUG_SEND_MESSAGE
+	#include "config.h"
+	#include "joystick.h"
+#endif
 
 int STOP = FALSE;
 
@@ -23,21 +34,68 @@ int main (void) {
 	// Initialise interrupts
 	initSig(fd);
 
-	// Joystick input, needs to come here!
-	char msg[4] = "a52b";
-
-
-	// send message to FPGA
-	send(msg, 4);
-
 	// Place the received message here
 	extern char rMsg[255];
 	extern int i;
 
-	// receive message from FPGA
-	while(rMsg[i-1] != 'b') {		// TODO CHANGE THIS!! can cause a outofbound error
-		sleep(500);
-	}
+	/************************************************************
+	*	Send/receive a test message and print it to the screen
+	*************************************************************/
+	#if DEBUG_SEND_MESSAGE
+		// Test message!
+		char msg[4] = "a52b";
+
+		// send message to FPGA
+		send(msg, 4);
+
+		// receive message from FPGA
+		while(rMsg[i-1] != '2') {		// TODO CHANGE THIS!! can cause a outofbound error
+			sleep(500);
+		}
+
+	/************************************************************
+	*	Get Joystick input and send it to the QR
+	*************************************************************/
+	#else
+		/* current axis and button readings
+		 */
+		int	axis[6];
+		int	button[12];
+		int js_fd;
+		struct js_event js;
+		struct js_event *js_ptr = &js;
+		unsigned int t;
+
+		//open and configure the joystick
+		js_fd = configure_joystick();
+
+		while (1) {
+			/* simulate work
+			 */
+			mon_delay_ms(300);
+			t = mon_time_ms();
+
+			//read out the joystick values
+			read_joystick(js_fd, &js, axis, button);
+
+			//print the joystick values along with the time
+			print_joystick(axis, button,t);
+
+			// Put data from joystick into a message
+			*(axis + ROLL);
+			*(axis + PITCH);
+			*(axis + YAW);
+			*(axis + LIFT);
+
+
+			// Send data
+			//send();
+
+			if (button[FIRE])
+				break;
+		}
+	#endif
+
 	rMsg[i] = '\0';
 	printf("FINAL:\t%s\n", rMsg);
 
