602f31dc9aa65cabb7f30ea8c792c083f409e9f1
Bastiaan Oosterhuis <bastiaanoosterhuis@hotmail.com>
2015-09-10 09:28:19 +0200

    Multiplication and division work as well. Only need to check rounding

--- fixed point/fixed_point.h
+++ fixed point/fixed_point.h
@@ -1,15 +1,20 @@
 #ifndef FIXED_POINT_H
 #define FIXED_POINT_H 
 
+/* Fixed point macros
+ * Author: Bastiaan Oosterhuis
+ *	
+ */
+
 /*
 	Qm.n format
 	m bits in total(32)
-	n bits for the fration part
+	n bits for the fraction part
 	Resolution is 2^-Fract_part
 */
-#define INT_PART	16
-#define FRACT_PART	16
-#define FACTOR 		65536	//2^FRACT_PART
+#define INT_PART	8	
+#define FRACT_PART	8
+#define FACTOR 		256	//2^FRACT_PART
 
 /*
 	Conversions:
@@ -17,14 +22,14 @@
 
 #define INT_TO_FIXED(A)   (((int)A) << FRACT_PART) 
 #define FIXED_TO_INT(A)   ((int)(A >> 8))
-#define FP_TO_FIXED(A) ((int)(A*FACTOR))  
-#define FIXED_TO_FP(A) (((float)A)/FACTOR)   
+#define FP_TO_FIXED(A) ((int)(A * FACTOR))  
+#define FIXED_TO_FP(A) (((float)A) / FACTOR)   
 
 /*
 	Operations
 */
-#define MULT_FIXED(A,B) = ((int)(A * B) >> FRACT_PART)
-#define DIV_FIXED(A,B) = (int)((A << FRACT_PART)/B)
+#define MULT_FIXED(A,B)  ((int)(A * B) >> FRACT_PART)
+#define DIV_FIXED(A,B)  (int)((A << FRACT_PART)/B)
 
 
 #endif
