61508b6d7bce8b5e827ce4affecc1de5129114b3
Bastiaan Oosterhuis <bastiaanoosterhuis@hotmail.com>
2015-10-13 11:27:00 +0200

    added extra brackets to the fixed point macros

--- x32/fixed_point.h
+++ x32/fixed_point.h
@@ -14,26 +14,26 @@
 	 Conversions
 */
 
-#define I2FDP(A)  		 	(((int)A) << FRACT_PART) 
-#define I2FDP_S(A,SIZE) 	(((int)A) << SIZE) 
+#define I2FDP(A)  		 	(((int)(A)) << FRACT_PART) 
+#define I2FDP_S(A,SIZE) 	(((int)(A)) << SIZE) 
 
-#define FDP2I(A)			((int)(A + 512) >> FRACT_PART)//((int)(A + 256) >> FRACT_PART)
-#define FDP2I_S(A,SIZE) 	((int)(A+ (1<<(SIZE-1))) >> SIZE)//((int)(A + 256) >> SIZE)
+#define FDP2I(A)			((int)((A) + 512) >> FRACT_PART)//((int)(A + 256) >> FRACT_PART)
+#define FDP2I_S(A,SIZE) 	((int)((A)+ (1<<(SIZE-1))) >> SIZE)//((int)(A + 256) >> SIZE)
 
-#define FP2FDP(A) 			((int)(A * FACTOR))  
-#define FP2FDP_S(A,SIZE) 	((int)(A * (1<<SIZE)))  
+#define FP2FDP(A) 			((int)((A) * FACTOR))  
+#define FP2FDP_S(A,SIZE) 	((int)((A) * (1<<SIZE)))  
 
-#define FDP2FP(A) 			(((float)A) / FACTOR)   
-#define FDP2FP_S(A,SIZE) 	(((float)A) / (1<<SIZE)) 
+#define FDP2FP(A) 			(((float)(A)) / FACTOR)   
+#define FDP2FP_S(A,SIZE) 	(((float)(A)) / (1<<SIZE)) 
 
 /*
 	Operations
 */
-#define MULT(A,B)  			((int)(A * B) >> FRACT_PART)
-#define MULT_S(A,B,SIZE) 	((int)(A * B) >> SIZE)
+#define MULT(A,B)  			((int)((A) * (B)) >> FRACT_PART)
+#define MULT_S(A,B,SIZE) 	((int)((A) * (B)) >> SIZE)
 
 #define DIV(A,B) 			 (int)(((A) << FRACT_PART)/(B))
-#define DIV_S(A,B,SIZE)  	((int)(( A << SIZE)/B))
+#define DIV_S(A,B,SIZE)  	((int)(((A) << SIZE)/(B)))
 
 
 #endif
